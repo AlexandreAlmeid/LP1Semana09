cceabe3d51d37ee14c4a78be3a63e300cd1a81d8
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-05-10 19:29:17 +0100

    FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamWriter file = new StreamWriter(args[0]);
+            string line;
+            while (true)
+            {
+                line = Console.ReadLine();
+                if (String.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+                file.WriteLine(line);
+            }
+        }
+    }
+}
