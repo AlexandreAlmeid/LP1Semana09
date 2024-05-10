42c43a104e1ef2eaadbf6e371da6d63d843183cf
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-05-10 20:32:01 +0100

    CharSets

---
+++ CharSets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CharSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<char>[] filesets = new HashSet<char>[args.Length];
+            string[] lines;
+            for (int i = 0; i < args.Length; i++)
+            {
+                filesets[i] = new HashSet<char>();
+                lines = File.ReadAllLines(args[i]);
+                foreach (string line in lines)
+                {
+                    if (line.Length > 1)
+                    {
+                        Environment.ExitCode = -1;
+                    }
+                    filesets[i].Add(Char.Parse(line));
+                }
+            }
+            HashSet<char> result = filesets[0];
+            foreach (HashSet<char> set in filesets)
+            {
+                result.IntersectWith(set);
+            }
+            List<char> final = result.ToList();
+            final.Sort();
+            foreach (char c in final)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
