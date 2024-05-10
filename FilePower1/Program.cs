70fa6e4e3490401d845cb77ff9d87dd0354550b9
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-05-10 19:17:45 +0100

    FilePower1

---
+++ FilePower1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> text = new Queue<string>();
+            string line;
+            while (true)
+            {
+                line = Console.ReadLine();
+                if (String.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+                text.Enqueue(line);
+            }
+            File.WriteAllLines(args[0], text);
+        }
+    }
+}
